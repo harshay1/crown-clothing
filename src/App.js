d200d6acdba5771fe156a94b8fb37570e6ac3103
Harsha Yalvarthy <harsha.hy@gmail.com>
2020-07-13 13:05:51 -0500

    Adding Header Components and links to home, logo and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import {HomePage} from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        {/* <HomePage /> */}
+        <Header />
+        <Switch>  
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
     </div>
   );
 }
