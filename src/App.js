089425aa491fc2d202a13644ef01e3322fd31e8a
Harsha Yalvarthy <harsha.hy@gmail.com>
2020-07-13 12:45:28 -0500

    Updating our App with Shop page, Introduce Routing, Collection Preview and Item Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import {HomePage} from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
 
 function App() {
   return (
     <div>
-        <HomePage />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        {/* <HomePage /> */}
     </div>
   );
 }
