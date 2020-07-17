e3797e885746d066f3855aa64208f7d1d8df5811
Harsha Yalvarthy <harsha.hy@gmail.com>
2020-07-16 22:48:40 -0500

    Removed the old state management included redux and implemented userReducer userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
+import { connect } from "react-redux";
 import {HomePage} from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import './App.css';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/signin-signup/signin-signup.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unSubscribeFromAuth = null
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unSubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapshot => {
           // console.log(snapshot.data());
-            this.setState({
-              currentUser: {
+            setCurrentUser({
                 id: snapshot.id,
                 ...snapshot.data()
-              }
-            }, ()=> {
-              // console.log(this.state);
             });
+            // console.log(this.state);
         });
       }
-      this.setState({currentUser: userAuth});
+      setCurrentUser({userAuth});
     });
-    
   }
 
   componentWillUnmount(){
@@ -45,7 +37,7 @@ class App extends React.Component {
   render() {
       return (
         <div>
-            <Header currentUser={this.state.currentUser} />
+            <Header/>
             <Switch>  
               <Route exact path='/' component={HomePage} />
               <Route path='/shop' component={ShopPage} />
@@ -56,4 +48,8 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapDispatchProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchProps)(App);
