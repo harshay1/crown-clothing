7b5dbe1fa1209c38c3203592d55e39bf92e0c68d
Harsha Yalvarthy <harsha.hy@gmail.com>
2020-07-17 12:03:56 -0500

    Created a Reducer flow to load Shop Collections data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { createStructuredSelector} from "reselect";
 import './shop.styles.scss';
-import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { selectCollections } from '../../redux/shop/shop.selector';
 
-class ShopPage extends React.Component{
+const ShopPage =({collections}) => {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+    return (
+        <div className='shopPage'>
+            {
+                collections.map( ({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+            
+        </div>
+    );
+};
 
-    render(){
-        const {collections} = this.state;
-        return (
-            <div className='shopPage'>
-                {
-                    collections.map( ({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-                
-            </div>
-        );
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+  });
 
-    }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import CartReducer from "./cart/cart.reducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import DirectoryReducer from "./directory/directory.reducer";
+import ShopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
     key: 'root',
@@ -14,7 +15,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: UserReducer,
     cart: CartReducer,
-    directory: DirectoryReducer
+    directory: DirectoryReducer,
+    shop: ShopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
